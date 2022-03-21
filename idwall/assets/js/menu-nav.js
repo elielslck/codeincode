59f1a801633900ef1c3148b9b9d1b6899d8381fd
Igor Bueno <igor.sanchez.bueno@gmail.com>
2022-03-20 21:08:02 -0300

    Implementação do aside e menu hamburguer

---
+++ idwall/assets/js/menu-nav.js
@@ -0,0 +1,37 @@
+class MenuNavBar{
+    constructor(mobileMenu,navList,navLinks){
+        this.mobileMenu = document.querySelector(mobileMenu);
+        this.navList = document.querySelector(navList);
+        this.navLinks = document.querySelector(navLinks);
+        this.activeClass = "active";
+
+        this.handleClick = this.handleClick.bind(this);
+}
+
+handleClick(){
+        this.navList.classList.toggle(this.activeClass);
+        this.mobileMenu.classList.toggle(this.activeClass);
+        
+    }
+
+    addClickEvent(){
+        this.mobileMenu.addEventListener("click",this.handleClick);
+    }
+
+    init(){
+        if (this.mobileMenu){
+            this.addClickEvent();
+        }
+        return this;
+    }
+
+}
+
+const menuNavBar = new MenuNavBar(
+    ".mobile-menu",
+    ".nav-list",
+    ".nav-list li",
+);
+
+menuNavBar.init();
+
